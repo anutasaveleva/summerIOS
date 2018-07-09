c092d3917ced41f55050821e343a3a76794f7d60
itisioslab <itisioslab@mac-mini-administrator.local>
2018-07-09 10:03:24 +0300

    Initial Commit

---
+++ summerpractTests/summerpractTests.swift
@@ -0,0 +1,36 @@
+//
+//  summerpractTests.swift
+//  summerpractTests
+//
+//  Created by itisioslab on 09.07.2018.
+//  Copyright © 2018 itisioslab. All rights reserved.
+//
+
+import XCTest
+@testable import summerpract
+
+class summerpractTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
